e96bad5a4b1f29a0260c5b78ebfbb8381bc5a98d
Dmex87 <103760478+Dmex87@users.noreply.github.com>
2022-04-16 03:21:08 +0700

    Update Nadyasb.py

--- Nadyasb.py
+++ Nadyasb.py
@@ -13,7 +13,7 @@ botStart = time.time()
 
 nadya = LINE()
 #nadya = LINE("TOKEN KAMU")
-#nadya = LINE("Email","Password")
+#nadya = LINE("iyanhuriansyah@gmail.com","iyan209776")
 nadya.log("Auth Token : " + str(nadya.authToken))
 channelToken = nadya.getChannelResult()
 nadya.log("Channel Token : " + str(channelToken))
